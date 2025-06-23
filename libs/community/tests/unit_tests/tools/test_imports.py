9bcbf9167ba2038ae6217e14597073934173a190
Saransh-Shankar <saransh.shankar@camb.ai>
2025-06-23 18:11:47 +0000

    Added test
    Signed-off-by: Saransh-Shankar <saransh.shankar@camb.ai>

--- libs/community/tests/unit_tests/tools/test_imports.py
+++ libs/community/tests/unit_tests/tools/test_imports.py
@@ -69,6 +69,7 @@ EXPECTED_ALL = [
     "GoogleSearchRun",
     "GoogleSerperResults",
     "GoogleSerperRun",
+    "GoogleVertexCambTool",
     "HumanInputRun",
     "IFTTTWebhook",
     "IncomeStatements",
