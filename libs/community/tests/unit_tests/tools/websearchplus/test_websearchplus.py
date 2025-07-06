a1a2cc1ec8719c7313ea915de50ff3e806b676e6
websearchplus <admin@websearch.plus>
2025-07-06 22:33:26 +0800

    Update test_websearchplus.py

--- libs/community/tests/unit_tests/tools/websearchplus/test_websearchplus.py
+++ libs/community/tests/unit_tests/tools/websearchplus/test_websearchplus.py
@@ -7,9 +7,10 @@ from langchain_community.utilities.websearchplus_search import WebSearchPlusAPIW
 os.environ["WEBSEARCHPLUS_API_KEY"] = "your_api_key_here"  # Replace with your actual API key for testing
 
 class TestWebSearchPlusTool(unittest.TestCase):
+
     @patch(
-        "langchain_community.tools.websearchplus.tool.WebSearchPlusResults.invoke",
-        return_value=[{"title": "Test Result", "link": "https://example.com", "snippet": "This is a test result."}],
+        "langchain_community.tools.websearchplus.tool.WebSearchPlusResults._run",
+        return_value=[{"content": "High-relevance Result", "url": "https://example.com", "title": "This is a title of result.", "score": 0.95}],
     )
     def test_invoke(self, mock_run):
         query = "langchain"
@@ -19,7 +20,7 @@ class TestWebSearchPlusTool(unittest.TestCase):
         websearchplus_tool = WebSearchPlusResults(api_wrapper=api_wrapper)  # type: ignore[call-arg]
         input_dict = input_data.model_dump(exclude_unset=True, exclude_none=True)
         results = websearchplus_tool.invoke(input = input_dict)
-        expected_result = [{"title": "Test Result", "link": "https://example.com", "snippet": "This is a test result."}]
+        expected_result = [{"content": "High-relevance Result", "url": "https://example.com", "title": "This is a title of result.", "score": 0.95}]
         self.assertEqual(results, expected_result)
 
 if __name__ == '__main__':
