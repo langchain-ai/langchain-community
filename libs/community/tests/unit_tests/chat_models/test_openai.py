f99c9c778c185245bb72b1ac18ce6b4772ea9298
Chester Curme <chester.curme@gmail.com>
2025-10-07 14:46:08 -0400

    apredict -> ainvoke

--- libs/community/tests/unit_tests/chat_models/test_openai.py
+++ libs/community/tests/unit_tests/chat_models/test_openai.py
@@ -128,6 +128,6 @@ async def test_openai_apredict(mock_completion: dict) -> None:
         "async_client",
         mock_client,
     ):
-        res = await llm.apredict("bar")
+        res = await llm.ainvoke("bar")
         assert res == "Bar Baz"
     assert completed
