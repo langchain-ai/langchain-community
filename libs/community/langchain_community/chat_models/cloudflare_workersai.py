0a369fad147b5ac2e8eeea25b130a52d802bd701
Chester Curme <chester.curme@gmail.com>
2025-10-07 14:14:22 -0400

    update bind_tools signature

--- libs/community/langchain_community/chat_models/cloudflare_workersai.py
+++ libs/community/langchain_community/chat_models/cloudflare_workersai.py
@@ -185,7 +185,7 @@ class ChatCloudflareWorkersAI(BaseChatModel):
         self,
         tools: Sequence[Union[Dict[str, Any], Type, Callable[..., Any], BaseTool]],
         **kwargs: Any,
-    ) -> Runnable[LanguageModelInput, BaseMessage]:
+    ) -> Runnable[LanguageModelInput, AIMessage]:
         """Bind tools for use in model generation."""
         formatted_tools = [convert_to_openai_tool(tool) for tool in tools]
         return super().bind(tools=formatted_tools, **kwargs)
