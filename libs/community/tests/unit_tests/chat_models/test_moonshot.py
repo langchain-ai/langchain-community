23dda6c6f1dbdeec35bf66b9067ce88588ced4b2
markshao <shaoyuliang@qq.com>
2025-11-22 23:28:34 +0800

    fix lint issues

--- libs/community/tests/unit_tests/chat_models/test_moonshot.py
+++ libs/community/tests/unit_tests/chat_models/test_moonshot.py
@@ -1,5 +1,6 @@
+from typing import Any
+
 import pytest
-from langchain_core.runnables import Runnable
 
 from langchain_community.chat_models.moonshot import MoonshotChat
 
@@ -9,5 +10,5 @@ mock_tool_list = [lambda: f"tool-id-{i}" for i in range(3)]
 @pytest.mark.requires("openai")
 def test_moonshot_bind_tools() -> None:
     llm = MoonshotChat(name="moonshot")
-    ret: Runnable = llm.bind_tools(mock_tool_list)
+    ret: Any = llm.bind_tools(mock_tool_list)
     assert len(ret.kwargs["tools"]) == 3
